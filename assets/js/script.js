1fdb34f46f1ba60d9a731f245f47007a4fa0b849
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-12 18:40:05 +0000

    added variables for queryURL and api key, and the fetch request to pull the weather data. I have just used London for now but will make the area dynamic later on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log('ding')
+const APIKey = "34868cf202b6a50a7276328f1ce106bf";
+
+const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=London,Englandi&appid=${APIKey}`;
+
+fetch(queryURL)
+  .then(function (response) {
+    console.log(response)
+    return response.json();
+  }).then(function (data) {
+    console.log(data)
+
+
+
+
+
+
+
+
+    
+  })
+
