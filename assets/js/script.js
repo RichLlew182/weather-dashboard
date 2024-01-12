eda192ecb62d49d3f858fad1d21fa8c36780d49b
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-12 18:52:15 +0000

    was getting city not found errors because I need to first find the lat and long values of the city and then do another fetch request with those values on a different URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
-$('#search-button').on('click', function (e) {
+$('#search-button').on('click', function (e, userLocation) {
   
   e.preventDefault();
 
+  const APIKey = "34868cf202b6a50a7276328f1ce106bf";
+  
+  var userLocation = $('#search-input').val().trim()
 
-const APIKey = "34868cf202b6a50a7276328f1ce106bf";
-
-const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=London,Englandi&appid=${APIKey}`;
-
-fetch(queryURL)
-  .then(function (response) {
-    console.log(response)
-    return response.json();
-  }).then(function (data) {
-    console.log(data)
+ 
+const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=${APIKey}`;
 
 
-
+fetch(geoURL)
+.then(function (response) {
+  console.log(response)
+  return response.json();
+}).then(function (data) {
+  console.log(data)
+  
+  
+  
+  // const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userLocation}i&appid=${APIKey}`;
 
 
 
