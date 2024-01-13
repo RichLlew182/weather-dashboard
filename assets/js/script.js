7cd98eed86023fad17635280d3ec9224b806c2ef
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-13 09:26:57 +0000

    added a click function to the end of the doc that checks for all buttons with the history-btn class, and I have added some console logs to check that everything works

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,87 +15,99 @@ if (storedHistory) {
 
 }
 
+
 $('#search-button').on('click', function (e, userLocation) {
   
-  e.preventDefault();
-  var userLocation = $('#search-input').val().trim()
+    e.preventDefault();
+    var userLocation = $('#search-input').val().trim()
 
-  if (storedHistory) {
-    console.log(storedHistory)
-    storedHistory.push(userLocation)
-    localStorage.setItem('history', JSON.stringify(storedHistory));
+    if (storedHistory) {
+      console.log(storedHistory)
+      storedHistory.push(userLocation)
+      localStorage.setItem('history', JSON.stringify(storedHistory));
   
-    historyButton = $('<button>');
-    historyButton.text(userLocation).addClass('history-btn')
+      historyButton = $('<button>');
+      historyButton.text(userLocation).addClass('history-btn')
 
-    historyDiv = $('#history');
-    historyDiv.append(historyButton)
+      historyDiv = $('#history');
+      historyDiv.append(historyButton)
 
-  }
+    }
 
-  const APIKey = "34868cf202b6a50a7276328f1ce106bf";
+    const APIKey = "34868cf202b6a50a7276328f1ce106bf";
   
 
-const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=${APIKey}`;
+    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=${APIKey}`;
 
-fetch(geoURL)
-.then(function (response) {
-  console.log(response)
-  return response.json();
-}).then(function (data) {
-  console.log(data)
+    fetch(geoURL)
+      .then(function (response) {
+        console.log(response)
+        return response.json();
+      }).then(function (data) {
+        console.log(data)
   
-  var lat = data[0].lat;
-  var lon = data[0].lon;
+        var lat = data[0].lat;
+        var lon = data[0].lon;
 
-  console.log(lat,lon)
+        console.log(lat, lon)
   
-  const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIKey}`;
+        const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${APIKey}`;
 
-  fetch(queryURL)
-  .then(function (response) {
-    console.log(response)
-    return response.json();
-  }).then(function (newData) {
-    console.log(newData);
+        fetch(queryURL)
+          .then(function (response) {
+            console.log(response)
+            return response.json();
+          }).then(function (newData) {
+            console.log(newData);
 
-    const cityName = newData.city.name;
-    console.log({ cityName })
+            const cityName = newData.city.name;
+            console.log({ cityName })
     
-    let date = newData.list[0].dt_txt;
-    console.log({ date });
-    date = dayjs(date).format('DD/MM/YYYY')
-    console.log({ date });
+            let date = newData.list[0].dt_txt;
+            console.log({ date });
+            date = dayjs(date).format('DD/MM/YYYY')
+            console.log({ date });
 
-    const iconCode = newData.list[0].weather[0].icon;
-    console.log({ iconCode });
+            const iconCode = newData.list[0].weather[0].icon;
+            console.log({ iconCode });
 
-    const weatherIconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-    console.log({ weatherIconURL });
+            const weatherIconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            console.log({ weatherIconURL });
     
-    const temp = newData.list[0].main.temp
-    console.log({ temp })
+            const temp = newData.list[0].main.temp
+            console.log({ temp })
     
-    const windSpeed = newData.list[0].wind.speed
-    console.log({ windSpeed })
+            const windSpeed = newData.list[0].wind.speed
+            console.log({ windSpeed })
 
-    const humidity = newData.list[0].main.humidity
-    console.log({ humidity })
+            const humidity = newData.list[0].main.humidity
+            console.log({ humidity })
 
-    var todayDiv = $('#today');
-    var todayCard = $('<article>').addClass('card p-3')
-    var todayHeader = $('<h2>').text(cityName + ' ' + date);
-    var todayIcon = $('<img>').attr('src', weatherIconURL);
-    var todayTemp = $('<p>').text('Temp: ' + temp + ' °C')
-    var todayWind = $('<p>').text('Wind: ' + windSpeed + ' MPH')
-    var todayHumidity =  $('<p>').text('Humidity: ' + humidity + '%')
+            var todayDiv = $('#today');
+            var todayCard = $('<article>').addClass('card p-3')
+            var todayHeader = $('<h2>').text(cityName + ' ' + date);
+            var todayIcon = $('<img>').attr('src', weatherIconURL);
+            var todayTemp = $('<p>').text('Temp: ' + temp + ' °C')
+            var todayWind = $('<p>').text('Wind: ' + windSpeed + ' MPH')
+            var todayHumidity = $('<p>').text('Humidity: ' + humidity + '%')
 
-    todayCard.append(todayHeader, todayIcon, todayTemp, todayWind, todayHumidity);
-    todayDiv.append(todayCard)
+            todayCard.append(todayHeader, todayIcon, todayTemp, todayWind, todayHumidity);
+            todayDiv.append(todayCard)
 
-  })
+          })
 
+      })
+  
 })
   
-})
+
+$(document).on('click', '.history-btn', function (e) {
+  console.log(e)
+  console.log(this)
+  console.log($(this).text())
+
+  
+} )
+
+
 
