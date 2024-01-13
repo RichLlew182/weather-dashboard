283a91a353879997589d40b1d7663b11054be690
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-13 09:07:24 +0000

    added a variable that checks if there is any data stored locally, and if not is set as an empty array, then added an if statement that checks if there is existing data and pushes the new user location to it, and stores it in the local array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
+var storedHistory = localStorage.getItem('history') || [];
+
 $('#search-button').on('click', function (e, userLocation) {
   
   e.preventDefault();
+  var userLocation = $('#search-input').val().trim()
+
+  if (storedHistory) {
+    storedHistory.push(userLocation)
+
+    localStorage.setItem('history', JSON.stringify(storedHistory))
+  
+  }
 
   const APIKey = "34868cf202b6a50a7276328f1ce106bf";
   
-  var userLocation = $('#search-input').val().trim()
 
 const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&appid=${APIKey}`;
 
